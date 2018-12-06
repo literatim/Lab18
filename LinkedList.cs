84af65945764625100a021ff9c3efda0603af9ad
Ben Jen <benjendrzejak@quickenloans.com>
2018-12-05 21:58:06 -0500

    Laid framework

---
+++ LinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab18
+{
+    public static class LinkedListExtensions
+    {
+        public static bool RemoveAt(this LinkedList<int> list, int index)
+        {
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        public static void PrintReverse(this LinkedList<int> list)
+        {
+
+        }
+
+        public static bool InsertAt(this LinkedList<int> list, int index, object o)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Lab18
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
