57109407aa3c0fd94810a4b223d9403fb31454ad
James Jackson <James@Grandcircus.co>
2018-12-16 21:27:21 -0500

    JAmes TA Review

--- Program.cs
+++ Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
-using System.Linq;
 
 namespace Lab18
 {
-    class Program
-    {
-        const int size = 11;
+	class Program
+	{
+		const int size = 11;
 
-        static void Main(string[] args)
-        {
+		static void Main(string[] args)
+		{
 
-            int[] arrayOne = {2, 4, 4, 90, 41, 64, 100, 1, 1, 16, 17}; 
+			int[] arrayOne = { 2, 4, 4, 90, 41, 64, 100, 1, 1, 16, 17 };
 
-            Console.Write($"Array = {arrayOne}");
+			Console.Write($"Array = {arrayOne}");
 
-            RunCounterAlgorithm(arrayOne);
+			RunCounterAlgorithm(arrayOne);
 
-            Console.ReadKey();
-        }
+			Console.ReadKey();
+		}
 
-        static void RunCounterAlgorithm(int[] arrayOne)
-        {
-           
-        }
-    }
+		// James -- Everything in your linked list class was looking good, then I saw this... not sure what you are going  for here because
+		// this is an empty method my dude.
+		static void RunCounterAlgorithm(int[] arrayOne)
+		{
+
+		}
+	}
 }
